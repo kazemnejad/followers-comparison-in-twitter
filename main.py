f2f284bfe3f1c0d94ab69732acecaffe84e84a04
AmirHosein KazemNejad <ub.maka@gmail.com>
2018-04-27 12:03:17 +0430

    Update main.py
    Remove useless code

--- main.py
+++ main.py
@@ -11,26 +11,7 @@ def create_key_data(**kwargs):
 
 
 keys = [
-    create_key_data(consumer_key="SKQVjyHtPLJg1SnNwWs53m1GJ",
-                    consumer_secret="BBAonJUyMk1ME7ZxEFQDoyTz6QaGd090K1AOmPPRJ4t3oJ3TTF",
-                    access_token_key="1385307680-2w1fHhohycFia8Me1wpRpyLAQzFWbD02VTUl12X",
-                    access_token_secret="Jzx2KMuyYg1pJaRIk9OrQvYbcIaKs8gS8jWLJQORzkSmM",
-                    sleep_on_rate_limit=True),
-    create_key_data(consumer_key="5YPcFukXr2fyangT3ngsgTtsw",
-                    consumer_secret="VT1ZXeapqTt1eIxBn7PD8Pa0GYVCPDo5ObunNSzJirJE4cbDxX",
-                    access_token_key="964445515382476801-oUA9hX5uI3HlHQl0KbPr7Yj9zcvEkDs",
-                    access_token_secret="2XD9cDRbR6cOGCx6WhXw32k8MrzfjLBg7TiAUtFbaGRwN",
-                    sleep_on_rate_limit=True),
-    create_key_data(consumer_key="y2cCKajuXJp4t4AxlQudA9XaY",
-                    consumer_secret="xOqw2NMy3d4JCNkxSjMhoLyjCCyGIlcC4f236XQyZlKmoWoydq",
-                    access_token_key="2241200860-Phh04eiBA1CnXGZhAV31fM3lvAYltiQac2TmsyM",
-                    access_token_secret="IjfyOKXSjSHmvNKt70I0h1sTHRbMs074lXCykCmRJ7AQj",
-                    sleep_on_rate_limit=True),
-    create_key_data(consumer_key="7lNR91mKJuHaXBKtM5TbZf2Mx",
-                    consumer_secret="DWRzgqyX6gaHvvvMbyvzZGCZzATR58ZLbw53BWsiuZWW5zXPhU",
-                    access_token_key="1929901087-eN7OKtvwdPokPVhuX7kr0UgFJ0wfSeFf",
-                    access_token_secret="7L5zZDDVvDPaU2WYsLNzJ8YXQ2CZPNbdC2hgF3sUxTiWh",
-                    sleep_on_rate_limit=True)
+    #...
 ]
 
 # api = twitter.Api(**keys[0])
